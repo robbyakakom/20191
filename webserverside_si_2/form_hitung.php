c452f15f0fdeaf3cfcd6bafe67d4ead902775c5f
robbyakakom <robby.akakom@gmail.com>
2019-09-16 20:28:38 +0700

    2019-09-16

---
+++ webserverside_si_2/form_hitung.php
@@ -0,0 +1,5 @@
+<form action="hitung.php" method="post">
+  bilangan a <input type="text" name="a" > <br>
+  bilangan b <input type="text" name="b" > <br>
+  <button name="tambah" type="submit">Tambah</button>
+</form>
---
+++ webserverside_si_2/hitung.php
@@ -0,0 +1,11 @@
+<?php 
+$a = $_POST['a'] ;
+$b = $_POST['b'] ;
+
+$c = $a + $b ;
+
+echo "Proses Penambahan <br>" ;
+echo "bilangan a : " . $a . "<br>" ;
+echo "bilangan b : " . $b . "<br>" ;
+echo "hasil b : " . $c . "<br>" ;
+?>
