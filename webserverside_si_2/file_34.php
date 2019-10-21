26a9ba36420c2da21d14866d25edcca7727fd105
robbyakakom <robby.akakom@gmail.com>
2019-10-21 16:46:46 +0700

    2019-10-21

---
+++ webserverside_si_2/file_34.php
@@ -0,0 +1,22 @@
+<?php 
+//tangkap data dari form
+$nim = $_POST['nim'] ;
+$nama = $_POST['nama'] ;
+
+// 1. membuat koneksi
+$konek = mysqli_connect("localhost","root","","akakom") ;
+
+// 2. membuat sql string
+$sql = "insert into mhs(nim, nama) values ('$nim','$nama')" ;
+
+// 3. menjalankan query / sql
+$hasil = mysqli_query($konek, $sql) ;
+if($hasil) {
+  echo "Data berhasil disimpan" ;
+}else{
+  echo "Data gagal disimpan" ;
+}
+
+// 4. menutup koneksi
+mysqli_close($konek) ;
+?>
---
+++ webserverside_si_2/form_mhs.php
@@ -0,0 +1,6 @@
+<h2>Input Data Mahasiswa</h2>
+<form action="file_34.php" method="post">
+  NIM <br> <input type="text" name="nim" id="nim"> <br>
+  Nama <br> <input type="text" name="nama" id="nama"> <hr>
+  <button type="submit">SIMPAN</button>
+</form>
